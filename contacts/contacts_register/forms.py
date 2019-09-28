f44cdcbe9d5ffd3d045bfa4babed4194b22abb37
Willians Varela <willians.varela@gmail.com>
2019-09-28 00:05:26 -0300

    add subapp contacts_register

---
+++ contacts/contacts_register/forms.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+from django import forms
+from django.forms import ModelForm, Form
+
+# Create your forms here.
---
+++ contacts/contacts_register/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.conf.urls import url
+from django.urls import path, include
+
+
+urlpatterns = [
+
+]
