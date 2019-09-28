285180ffc2f8d242afe1489acff2278deb1fc70e
italocolt <italocoltact@gmail.com>
2019-09-28 15:43:21 -0300

    Animal Section CRUD interface improved

--- animal/views.py
+++ animal/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import AnimalForm, SpeciesForm, BreedForm, HealthLogForm, HealthForm, ShelterForm
+from .forms import AnimalForm, SpeciesForm, BreedForm, HealthLog, HealthLogForm, HealthForm, ShelterForm
 from .models import Animal, Shelter
 
 
@@ -8,12 +8,17 @@ def animal(request):
     animals = []
 
     for animal_obj in Animal.objects.all():
-        shelter_objs = Shelter.objects.filter(animal = animal_obj.id)
+        shelter_objs = Shelter.objects.filter(animal=animal_obj.id)
         if len(shelter_objs) > 0:
-            category = shelter_objs.order_by('-entry_date')[0].category
-            animal_obj.category = category
+            shelter_category = shelter_objs.order_by('-entry_date')[0].category
+            animal_obj.shelter_category = shelter_category
         else:
-            animal_obj.category = None
+            animal_obj.shelter_category = None
+
+        animal_obj.shelters = shelter_objs
+
+        healthlog_objs = HealthLog.objects.filter(animal=animal_obj.id).order_by('-date')
+        animal_obj.healthlogs = healthlog_objs
         animals.append(animal_obj)
 
     context = {'pagina': 'Lista de Animais', 'page_title': 'Animais'}
@@ -22,26 +27,25 @@ def animal(request):
 
 
 def new_animal(request):
-    data = {}
     animal_form = AnimalForm(request.POST or None)
-    healthlog_form = HealthLogForm(request.POST or None)
+    # shelter_form = ShelterForm(request.POST or None)
 
     if animal_form.is_valid():
         animal_obj = animal_form.save()
-        if healthlog_form.is_valid():
-            healthlog_obj = healthlog_form.save(False)
-            healthlog_obj.animal = animal_obj
-            healthlog_obj.save()
+        # shelter_obj = shelter_form.save(False)
+        # shelter_obj.animal = animal_obj
+        # shelter_obj.exit_date = None
+        # shelter_obj.save()
         return redirect('animal')
 
-    data['action'] = 'create'
-    data['animal_form'] = animal_form
-    data['healthlog_form'] = healthlog_form
-    return render(request, 'animal_form.html', data)
+    context = {'pagina': 'Novo Animal', 'page_title': 'Novo Animal'}
+    context['animal_form'] = animal_form
+    # context['shelter_form'] = shelter_form
+    # context['action'] = 'create'
+    return render(request, 'animal_form.html', context)
 
 
 def update_animal(request, pk):
-    data = {}
     animal_obj = Animal.objects.get(pk=pk)
     form = AnimalForm(request.POST or None, instance=animal_obj)
 
@@ -49,10 +53,11 @@ def update_animal(request, pk):
         form.save()
         return redirect('animal')
 
-    data['action'] = 'update'
-    data['animal_form'] = form
-    data['animal'] = animal_obj
-    return render(request, 'animal_form.html', data)
+    context = {'pagina': 'Editar Animal', 'page_title': 'Editar Animal'}
+    context['action'] = 'update'
+    context['animal_form'] = form
+    context['animal'] = animal_obj
+    return render(request, 'animal_form.html', context)
 
 
 def del_animal(request, pk):
@@ -62,27 +67,27 @@ def del_animal(request, pk):
 
 
 def new_species_popup(request):
-    data = {}
+    context = {}
     form = SpeciesForm(request.POST or None)
 
     if form.is_valid():
         instance = form.save()
         return HttpResponse('<script>opener.closePopup(window, "%s", "%s", "#id_species");</script>' % (instance.pk, instance))
 
-    data['form'] = form
-    return render(request, "species_form.html", data)
+    context['form'] = form
+    return render(request, "species_form.html", context)
 
 
 def new_breed_popup(request):
-    data = {}
     form = BreedForm(request.POST or None)
 
     if form.is_valid():
         instance = form.save()
         return HttpResponse('<script>opener.closePopup(window, "%s", "%s", "#id_breed");</script>' % (instance.pk, instance))
 
-    data['form'] = form
-    return render(request, "breed_form.html", data)
+    context = {'pagina': 'Nova Raça', 'page_title': 'Nova Raça'}
+    context['form'] = form
+    return render(request, "breed_form.html", context)
 
 
 def new_health_popup(request):
@@ -97,6 +102,20 @@ def new_health_popup(request):
     return render(request, "health_form.html", data)
 
 
+def new_healthlog_popup(request, pk):
+    healthlog_form = HealthLogForm(request.POST or None)
+
+    if healthlog_form.is_valid():
+        healthlog_obj = healthlog_form.save(False)
+        healthlog_obj.animal = Animal.objects.get(pk=pk)
+        healthlog_obj.save()
+        return HttpResponse('<script>opener.closePopup(window, "%s", "%s", "#id_healthlog");</script>' % (healthlog_obj.pk, healthlog_obj))
+
+    context = {'pagina': 'Nova Informação Clínica', 'page_title': 'Nova Informação Clínica'}
+    context['form'] = healthlog_form
+    return render(request, "healthlog_form.html", context)
+
+
 def shelter(request):
     context = {'pagina': 'Lista de Acolhimentos', 'page_title': 'Acolhimentos'}
     context['shelter'] = Shelter.objects.all()
@@ -104,19 +123,18 @@ def shelter(request):
 
 
 def new_shelter(request):
-    data = {}
     form = ShelterForm(request.POST or None)
 
     if form.is_valid():
         form.save()
         return redirect('shelter')
 
-    data['form'] = form
-    return render(request, 'shelter_form.html', data)
+    context = {'pagina': 'Novo Acolhimento', 'page_title': 'Novo Acolhimento'}
+    context['form'] = form
+    return render(request, 'shelter_form.html', context)
 
 
 def update_shelter(request, pk):
-    data = {}
     shelter_obj = Shelter.objects.get(pk=pk)
     form = ShelterForm(request.POST or None, instance=shelter_obj)
 
@@ -124,9 +142,10 @@ def update_shelter(request, pk):
         form.save()
         return redirect('shelter')
 
-    data['form'] = form
-    data['shelter'] = shelter_obj
-    return render(request, 'shelter_form.html', data)
+    context = {'pagina': 'Editar Acolhimento', 'page_title': 'Editar Acolhimento'}
+    context['form'] = form
+    context['shelter'] = shelter_obj
+    return render(request, 'shelter_form.html', context)
 
 
 def del_shelter(request, pk):
