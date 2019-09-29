3ecc5b2714a4e58469db44fab14f53edbbbd4f16
Willians Varela <willians.varela@gmail.com>
2019-09-29 01:43:26 -0300

    add create, read and delete to Contacts module

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.conf.urls import url
-from django.urls import path, include
+from django.urls import path
+from contacts.views import IndexView
+from contacts.contacts_register.views import register_contact, delete_contact
 
 app_name = 'contacts'
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='home'),
+    path('add', register_contact, name='create_contact'),
+    path('delete/<int:pk>', delete_contact, name='delete_contact'),
 
 ]
